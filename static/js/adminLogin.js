34a405b8fa5e4e81f25e5827e4514f0dc48fad25
mohab ghobashy <93479326+MohabGhobashy@users.noreply.github.com>
2023-07-07 10:00:03 +0300

    admin login edit

--- static/js/adminLogin.js
+++ static/js/adminLogin.js
@@ -1,14 +1,18 @@
-function togglePasswordVisibility() {
-    var passwordInput = document.getElementById("password");
-    var toggleIcon = document.querySelector(".toggle-password");
-
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-        toggleIcon.classList.remove("fa-eye-slash");
-        toggleIcon.classList.add("fa-eye");
-    } else {
-        passwordInput.type = "password";
-        toggleIcon.classList.remove("fa-eye");
-        toggleIcon.classList.add("fa-eye-slash");
+window.addEventListener("DOMContentLoaded", function () {
+    var adminInput = document.querySelectorAll(".adminInput");
+  
+    for (let input of adminInput) {
+      input.addEventListener("input", function () {
+        if (input.value !== "") {
+          input.style.border = "solid 2px rgba(15, 90, 170, 0.7)";
+        } else {
+          input.style.border = "solid 2px rgb(182, 182, 182)";
+        }
+      });
+  
+      input.addEventListener("invalid", function () {
+        input.style.border = "solid 2px rgba(211, 25, 15, 0.6)";
+      });
     }
-}
+  });
+  
