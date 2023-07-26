fd625fde5e15b0e0e33e80b5afa2bf5925c3bcb6
Mariam Ezzat <93602995+mariamezzat01@users.noreply.github.com>
2023-07-26 15:27:10 +0300

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -288,3 +288,25 @@ about.onclick = function () {
     Workshops.setAttribute("Class", "");
     about.setAttribute("Class", "active");
 }
+
+
+
+// numbers counter 
+
+const counts = document.querySelectorAll('.count')
+const speed = 97
+
+counts.forEach((counter) => {
+    function upDate(){
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        const inc = target / speed        
+        if(count < target){
+            counter.innerText = Math.floor(inc + count) 
+            setTimeout(upDate, 15)
+        }else{
+            counter.innerText = target
+        }
+    }
+    upDate()
+})
