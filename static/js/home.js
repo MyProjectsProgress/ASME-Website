2a0c4a78fec7598e9ca7c59c7a99543c44a78edf
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2023-08-06 14:18:45 +0300

    favicon in all htmls and uploads folder

--- static/js/home.js
+++ static/js/home.js
@@ -131,7 +131,7 @@ function addWorkshop(workshop) {
     let descriptionText = document.createTextNode(workshop.description);
     let registerText = document.createTextNode("Join now");
 
-    workshop_img.src = "/static/images/event1.jpeg";
+    workshop_img.src = workshop.image;
     title.appendChild(titleText);
     description.appendChild(descriptionText);
     register_btn.appendChild(registerText);
